dbffdd7e497b51e5ce96aee9f842256da9e04418
Jade Ferguson <jadesnider2440@gmail.com>
2022-08-02 14:06:53 -0700

    Added example to select all vendors

--- EntityFrameworkQueries/Form1.cs
+++ EntityFrameworkQueries/Form1.cs
@@ -6,5 +6,19 @@ namespace EntityFrameworkQueries
         {
             InitializeComponent();
         }
+
+        private void btnSelectAllVendors_Click(object sender, EventArgs e)
+        {
+            // if use same context through out the whole project it can actually be a problem as it can track entities
+            // can get messy if you have a bunch of different forms
+            using APContext dbContext = new APContext(); // using is an object to make sure things get disposed of properly
+
+            // LINQ (Language Integrated Query) method syntax
+            List<Vendor> vendorList = dbContext.Vendors.ToList(); // query database right away
+
+            // LINQ query syntax
+            List<Vendor> vendorList2 = (from v in dbContext.Vendors
+                                       select v).ToList();
+        }
     }
 }
